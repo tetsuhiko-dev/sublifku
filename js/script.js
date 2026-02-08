94eb49dc14032a729c53fff42d8f51b575325e76
tetsuhiko_dev <bousquieres.benjamin@gmail.com>
2026-02-08 14:43:49 +0100

    Put good url

--- js/script.js
+++ js/script.js
@@ -2,10 +2,22 @@ let mouseCursor = null;
 let previous_color = null;
 let slot_colors = [null, null, null, null];
 
+function get_slot_color_url(color) {
+    switch (color) {
+        case 'R':
+            return "imgs/shardRedFull.webp";
+        case 'G':
+            return "imgs/shardGreenFull.webp";
+        case 'B':
+            return "imgs/shardBlueFull.webp";
+        case 'W':
+            return "imgs/shardWhiteFull.webp";
+    }
+}
+
 function slot_color_click(color) {
     if (mouseCursor == null) {
         mouseCursor = document.querySelector(".slot_area");
-        console.log(mouseCursor);
     }
 
     switch (color) {
@@ -49,11 +61,9 @@ function slot_color_click(color) {
 }
 
 function change_slot_color(element) {
-    console.log(element.attributes.slot_index.value);
     if (slot_colors[element.attributes.slot_index.value] === previous_color) {
         element.src = "imgs/shardMultiEmpty.webp";
         slot_colors[element.attributes.slot_index.value] = null;
-        console.log(slot_colors);
         return;
     }
     switch (previous_color) {
@@ -71,7 +81,6 @@ function change_slot_color(element) {
             break;
     }
     slot_colors[element.attributes.slot_index.value] = previous_color;
-    console.log(slot_colors);
 }
 
 function compute_slot() {
@@ -81,4 +90,57 @@ function compute_slot() {
             return;
         }
     }
+
+    let slots = slot_colors[0] + slot_colors[1] + slot_colors[2] + slot_colors[3];
+
+    $.ajax({
+        url: "http://152.228.135.62:18080/compute/" + slots,
+        method: "GET",
+        success: function(data) {
+            let table_content = $("#table_content");
+            table_content.empty();
+            let entries = JSON.parse(data);
+            entries = entries.sort(function(a, b) {
+                // First, sort by fit_slot (true before false)
+                if (a.fit_slot !== b.fit_slot) {
+                    return b.fit_slot - a.fit_slot;
+                }
+                // Then, sort by sublimation_name alphabetically
+                return a.sublimation_name.localeCompare(b.sublimation_name);
+            });
+            for(let i=0; i<entries.length; i++) {
+                table_content.append(create_table_entry(entries[i]));
+            }
+        }
+    })
+}
+
+function create_table_entry(entry)
+{
+    return "<tr>" +
+        "<th scope='row'>" + entry.sublimation_name + "</th>" +
+        "<td>" + entry.fit_slot + "</td>" +
+        "<td>" + create_permutations(entry.equipment_slots) + "</td>" +
+    "</tr>";
+}
+
+function create_permutations(permutations)
+{
+    let permutations_html = "";
+    for(let i=0; i<permutations.length; i++) {
+        permutations_html += create_permutation(permutations[i]);
+    }
+    return permutations_html;
+}
+
+function create_permutation(permutation)
+{
+    return '' +
+        '<div class="justify-content-cednter permutation_area">' +
+            '<div class="permutation_body">' +
+                '<img class="permutation_slot_item permutation_slot_item_left" src="' + get_slot_color_url(permutation[0]) + '" alt=""/>' +
+                '<img class="permutation_slot_item permutation_slot_item_center" src="' + get_slot_color_url(permutation[1]) + '"  alt=""/>' +
+                '<img class="permutation_slot_item permutation_slot_item_right" src="' + get_slot_color_url(permutation[2]) + '" alt=""/>' +
+            '</div>' +
+        '</div>';
 }
